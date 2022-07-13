fe9601d88b8d794229f0e341fb39c88f276918f1
Woo Joo Chae <enaenen@naver.com>
2022-07-13 20:44:14 +0900

    ex00 complete

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define MEGAPHONE_MSG "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+
+int	main(int argc, char **argv){
+	std::string str;
+	if (1 < argc)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string input(argv[i]);
+			for (std::size_t j = 0; j < input.length(); j++)
+				str += std::toupper(input[j]);
+		}
+	}
+	else
+		str = MEGAPHONE_MSG;
+	std::cout << str << std::endl;
+	return (0);
+}
